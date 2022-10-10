b12628cb901d218290890a71bfbc4dfabf67f6be
dhivya1722 <83159474+dhivya1722@users.noreply.github.com>
2022-10-10 16:30:39 +0530

    Add files via upload

---
+++ ibmassignment2/dhivya/updatedelete.sql
@@ -0,0 +1,5 @@
+UPDATE user SET username = 'dhivya' WHERE rollno = '01';
+SELECT * FROM user;
+DELETE FROM user WHERE username = 'janani';
+SELECT * FROM user;
+
---
+++ ibmassignment2/dhivya/usertable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE user(emai1 varchar(20),username varchar(20),ro11no number(10),pswd number(10));
+INSERT INTO user (email,usemame,rol1no,pswd) VALUES ('dhivya@gmail.com', 'dhivya','41','123'); 
+INSERT INTO user (email,usemame,ro11no,pswd) VALUES ('janani@gmai1.com', 'janani','42','456'); 
+INSERT INTO user (email,username,rol1no,pswd) VALUES ('keerthana@gmai1.com', 'keerthana','51','789'); 
+INSERT INTO user (email,username,rol1no,pswd) VALUES ('kaviya@gmai1.com', 'kaviya','01','556');
